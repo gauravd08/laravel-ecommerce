1968261e293b948bc35a3c4deea48404ea511682
Gaurav Dhiman <gauravd08@gmail.com>
2019-04-03 23:17:03 +0530

    backend theme added with login,logout and change password

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,27 @@
 |
 */
 
-Route::get('/', 'Frontend\PagesController@home');
 
-Route::get('/user/createRoles', 'Auth\AuthController@createRoles');
-Route::get('/user/createUsers', 'Auth\AuthController@createUsers');
+// Route::get('/user/createRoles', 'Auth\AuthController@createRoles');
+// Route::get('/user/createUsers', 'Auth\AuthController@createUsers');
+
+//Public routes goes here...
+Route::get('/', 'Frontend\PagesController@home');
+Route::get('admin', 'Auth\AuthController@login');
+Route::post('login', 'Auth\AuthController@login');
+Route::get('logout', 'Auth\AuthController@logout');
+
+//admin routes
+Route::group(['prefix' => 'admin', 'middleware' => ['role:admin']], function()
+{
+    //Pages
+    Route::get('pages', 'Admin\PagesController@index')->name('pageSummary');
+    Route::any('ajax-pages', 'Admin\PagesController@ajaxIndex');
+    Route::any('pages/edit/{id}', 'Admin\PagesController@edit')->name('editPage');
+});
+
+//member routes
+Route::group(['prefix' => 'member', 'middleware' => ['role:member']], function()
+{
+    
+});
